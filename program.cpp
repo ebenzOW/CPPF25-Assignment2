d388f87fef0df4c04de35f2e7e77956833fd8280
ebenzOW <ebenz@oldwestbury.edu>
2025-11-13 06:04:24 +0000

    weird interpolation happening

--- program.cpp
+++ program.cpp
@@ -15,46 +15,39 @@ void draw_rectangle(BMP &bmp,int x, int y, int l, int h, int b, color c ){
         }
     }
 }
-void draw_rectangle_gradient(BMP &bmp, int x, int y, int l, int h, color c1, color c2)
+void draw_rectangle_gradient(BMP &bmp, int startX, int startY, int l, int h, color c1, color c2)
 {
 
-    if(l <= 0 || h <= 0);
-    color diff(c1.r - c2.r, c1.g - c2.g, c1.b - c2.b);
-    //int change_r = h /diff.r;
-    //int change_g = h /diff.g;
-    //int change_b = h /diff.b;
-    
-    
-    for(int i=0; i<=h; i++){
-        for(int ii= 0; ii<=l; i++)
+    if(l <= 0 || h <= 0) return;
+    float ratio;
+    uint8_t r,g,b;
+
+    for(int y=0; y<=h; y++){
+        ratio = ((y - startY)/(h - startY));
+        r = c1.r + ((c2.r - c1.r)*ratio);
+        g = c1.g + ((c2.g - c1.g)*ratio);
+        b = c1.b + ((c2.b - c1.b)*ratio); 
+
+        for(int x= 0; x<=l; x++)
         {
-            bmp.set_pixel(x + ii, y + i, )
+            bmp.set_pixel(startX + x, startY + y, r, g, b);
         }
     }
-
-
-
 
 }
 
 int main() {
     
-    BMP bmp(500, 500); 
-     srand(static_cast<unsigned int>(time(0)));
-    color c(0,255,0);
-    for(int i=0;i<5;i++){
-        uint8_t x=rand()%500;
-        uint8_t y=rand()%500;
-        uint8_t l=rand()%500;
-        uint8_t h=rand()%500;
-        
-        draw_rectangle(bmp,x,y,l,h,rand()%10,c);
-
-    }
+    BMP bmp(1000, 1000); 
     
-     draw_rectangle(bmp,1,1,500,500,10,color{255,0,0});
-
-    bmp.write("rect.bmp"); // Save the image to a file
+    draw_rectangle_gradient(bmp,1,1,1000,1000, color{255,192,203}, color{0,0,255});
+  
+    bmp.write("epic_gradient.bmp"); // Save the image to a file
 
     return 0;
 }
+
+
+//triangle
+//line formula using slope
+//(y = mx + b)
